d7b9ac5d5e8dc8e165711cfa3ad87182ad79138e
Egor <105357627+rightDesire@users.noreply.github.com>
2023-10-07 04:59:20 +0300

    Дорабатывает блоки Header и Hero

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,43 +1,60 @@
-const slider = document.querySelector('.slider__wrapper');
-const prevButton = document.querySelector('.slider__prev');
-const nextButton = document.querySelector('.slider__next');
-const slides = Array.from(slider.querySelectorAll('.slide'));
-const slideCount = slides.length;
-const paginationButtons = Array.from(document.querySelectorAll('.pagination__link'));
-const paginationButtonActive = document.querySelector('.pagination__link--current');
-let slideIndex = 0;
+const slider = document.querySelector('.slider')
+const nextButton = document.querySelector('.slider__next')
+const prevButton = document.querySelector('.slider__prev')
+const slides = Array.from(document.querySelectorAll('.slide'))
+const pagination = document.querySelector('.pagination')
+let currentIndex = 0;
 
-function showPreviousSlide() {
-  slideIndex ? slideIndex-- : true;
-  updateSlider();
+slider.classList.remove('slider--nojs')
+
+// Создаем кнопки пагинации
+slides.forEach((slide, index) => {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  li.classList.add('pagination__item');
+  button.classList.add('pagination__link');
+  li.appendChild(button);
+  button.addEventListener('click', () => {
+    setActiveSlide(index);
+  });
+  pagination.appendChild(li);
+});
+
+// Функция для установки активного слайда
+function setActiveSlide(index) {
+  slides.forEach((slide, slideIndex) => {
+    if (slideIndex === index) {
+      slide.style.display = 'block';
+      pagination.children[index].classList.add('pagination__item--active');
+    } else {
+      slide.style.display = 'none';
+      pagination.children[slideIndex].classList.remove('pagination__item--active');
+    }
+  });
+  currentIndex = index;
+
+  // Проверяем, нужно ли блокировать кнопки
+  prevButton.disabled = currentIndex === 0;
+  nextButton.disabled = currentIndex === slides.length - 1;
 }
 
-function showNextSlide() {
-  slideIndex !== slides.length ? slideIndex++ : true;
-  updateSlider();
-}
-
-function updateSlider() {
-  slideIndex <= 0 ? prevButton.setAttribute('disabled', true) : prevButton.removeAttribute('disabled', true);
-  (slideIndex + 1) === slides.length ? nextButton.setAttribute('disabled', true) : nextButton.removeAttribute('disabled', true);
-
-  paginationButtons.forEach((paginationButton) => {
-    paginationButton.classList.remove('pagination__link--current');
-  })
-
-  if (slideIndex > -1 && slideIndex !== slides.length) {
-    slides.forEach((slide, index) => {
-      if (index === slideIndex) {
-        slide.style.display = 'block';
-        paginationButtons[index].classList.add('pagination__link--current');
-      } else {
-        slide.style.display = 'none';
-      }
-    });
+// Функция для переключения на следующий слайд
+function nextSlide() {
+  if (currentIndex < slides.length - 1) {
+    setActiveSlide(currentIndex + 1);
   }
 }
 
-updateSlider();
+// Функция для переключения на предыдущий слайд
+function prevSlide() {
+  if (currentIndex > 0) {
+    setActiveSlide(currentIndex - 1);
+  }
+}
 
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
+// Обработчики событий для кнопок "Вперед" и "Назад"
+nextButton.addEventListener('click', nextSlide);
+prevButton.addEventListener('click', prevSlide);
+
+// По умолчанию устанавливаем первый слайд активным
+setActiveSlide(0);
